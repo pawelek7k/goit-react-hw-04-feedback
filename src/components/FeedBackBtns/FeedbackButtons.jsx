2acb4af56d0b6e4f6a0c381ba2086c0f27f493c2
pawelek <k.pawelek@vp.pl>
2024-04-10 18:16:59 +0200

    add stat

--- src/components/FeedBackBtns/FeedbackButtons.jsx
+++ src/components/FeedBackBtns/FeedbackButtons.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react";
+
 export const FeedbackButtons = ({ good = 0, neutral = 0, bad = 0 }) => {
+  const [feedback, setFeedback] = useState({ good, neutral, bad });
+
+  const handleFeedback = (type) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [type]: prevFeedback[type] + 1,
+    }));
+  };
+
   return (
     <>
-      <button>Good {good}</button>
-      <button>Neutral {neutral}</button>
-      <button>Bad {bad}</button>
+      <div>
+        {Object.keys(feedback).map((type) => (
+          <button key={type} onClick={() => handleFeedback(type)}>
+            {type.charAt(0).toUpperCase() + type.slice(1)}
+          </button>
+        ))}
+      </div>
+      <div>
+        <h2>Statistics</h2>
+        <ul>
+          {Object.entries(feedback).map(([type, value]) => (
+            <li key={type}>
+              {type.charAt(0).toUpperCase() + type.slice(1)}: {value}
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
